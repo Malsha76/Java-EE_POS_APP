6f78577dc2ba807023c1e9dbf83af7b7d8221819
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-08-27 20:32:16 +0530

    item

---
+++ Back_End/src/lk/ijse/pos/servlet/ItemServlet.java
@@ -0,0 +1,201 @@
+package lk.ijse.pos.servlet;
+
+import javax.json.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet(urlPatterns = {"/pages/item"})
+public class ItemServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "root123");
+            PreparedStatement pstm = connection.prepareStatement("select * from item");
+            ResultSet rst = pstm.executeQuery();
+            resp.addHeader("Content-Type","application/json");
+            resp.addHeader("Access-Control-Allow-Origin", "*");
+
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+
+            while (rst.next()) {
+
+                String code = rst.getString(1);
+                String description = rst.getString(2);
+                String qtyOnHand = rst.getString(3);
+                String unitPrice = rst.getString(4);
+
+                objectBuilder.add("code",code);
+                objectBuilder.add("description",description);
+                objectBuilder.add("qtyOnHand",qtyOnHand);
+                objectBuilder.add("unitPrice",unitPrice);
+
+                arrayBuilder.add(objectBuilder.build());
+
+            }
+
+            resp.getWriter().print(arrayBuilder.build());
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String code = req.getParameter("code");
+        String description = req.getParameter("description");
+        String qtyOnHand = req.getParameter("qtyOnHand");
+        String unitPrice = req.getParameter("unitPrice");
+
+        resp.addHeader("Content-Type","application/json");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "root123");
+
+            PreparedStatement pstm = connection.prepareStatement("insert into item values(?,?,?,?)");
+            pstm.setObject(1, code);
+            pstm.setObject(2, description);
+            pstm.setObject(3, qtyOnHand);
+            pstm.setObject(4, unitPrice);
+            if (pstm.executeUpdate() > 0) {
+
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("state", "OK");
+                objectBuilder.add("message", "Successfully Added.....");
+                objectBuilder.add("Data", " ");
+                resp.getWriter().print(objectBuilder.build());
+            }
+
+        } catch (ClassNotFoundException e) {
+
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("Data"," ");
+            resp.setStatus(500);
+            resp.getWriter().print(error.build());
+
+        } catch (SQLException e) {
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("Data"," ");
+            resp.getWriter().print(error.build());
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject customerObject = reader.readObject();
+
+        resp.addHeader("Content-Type","application/json");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods","PUT");
+
+        String code = customerObject.getString("code");
+        String description = customerObject.getString("description");
+        String qtyOnHand = customerObject.getString("qtyOnHand");
+        String unitPrice = customerObject.getString("unitPrice");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "root123");
+
+            PreparedStatement pstm = connection.prepareStatement("update Item set description=?,qtyOnhand=?,unitPrice=? where code=?");
+            pstm.setObject(4,code);
+            pstm.setObject(1,description);
+            pstm.setObject(2,qtyOnHand);
+            pstm.setObject(3,unitPrice);
+            boolean b = pstm.executeUpdate() > 0;
+
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            objectBuilder.add("state", "OK");
+            objectBuilder.add("message", "Successfully Updated.....");
+            objectBuilder.add("Data", " ");
+            resp.getWriter().print(objectBuilder.build());
+
+        } catch (ClassNotFoundException e) {
+
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("Data"," ");
+            resp.setStatus(500);
+            resp.getWriter().print(error.build());
+
+        } catch (SQLException e) {
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("Data"," ");
+            resp.getWriter().print(error.build());
+        }
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String code = req.getParameter("code");
+
+        resp.addHeader("Content-Type","application/json");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods","DELETE");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "root123");
+
+
+            PreparedStatement pstm2 = connection.prepareStatement("delete from item where code=?");
+            pstm2.setObject(1, code);
+            if (pstm2.executeUpdate() > 0) {
+
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("state", "OK");
+                objectBuilder.add("message", "Successfully Deleted....!");
+                objectBuilder.add("Data", " ");
+                resp.getWriter().print(objectBuilder.build());
+            }
+
+        } catch (ClassNotFoundException e) {
+
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("Data"," ");
+            resp.setStatus(500);
+            resp.getWriter().print(error.build());
+
+        } catch (SQLException e) {
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("state","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("Data"," ");
+            resp.getWriter().print(error.build());
+        }
+
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods","PUT");
+        resp.addHeader("Access-Control-Allow-Methods","DELETE");
+        resp.addHeader("Access-Control-Allow-Headers","Content-Type");
+    }
+}
